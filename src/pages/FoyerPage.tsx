eda3cafb3cdb36c1a62dfc2a39f8a1daeab1ad76
alextstlaurent <alex@lesalondesinconnus.com>
2026-08-17 10:20:18 -0400

    autosave: 2026-08-17 10:20:18

--- src/pages/FoyerPage.tsx
+++ src/pages/FoyerPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import {
   motion,
   AnimatePresence,
-  useScroll,
+  useMotionValue,
   useTransform,
   useReducedMotion,
 } from 'framer-motion';
