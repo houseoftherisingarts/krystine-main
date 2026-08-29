b82ec51f4ea7a189737c33c70dfcee60a913e3d9
alextstlaurent <alex@lesalondesinconnus.com>
2026-08-29 10:37:25 -0400

    autosave: 2026-08-29 10:37:25

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase-admin/app';
-export { importKajabiStructure } from "./importKajabi";
+export { importKajabiStructure, ingestMediaLecon } from "./importKajabi";
 
 initializeApp();
 
