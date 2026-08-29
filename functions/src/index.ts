827f17670946c2476ccf9921d55b248f3e8dec57
alextstlaurent <alex@lesalondesinconnus.com>
2026-08-28 21:00:01 -0400

    autosave: 2026-08-28 21:00:01

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,3 +12,4 @@ export { sendScheduledNewsletters, previewNewsletter } from './newsletter/send';
 export { newsletterAssistant } from './newsletter/assistant';
 export { verifierCaptcha } from './captcha';
 export { creerSessionPaiement, stripeWebhook, obtenirLecon } from './paiements';
+export { murVoteBillet, murVoteCommentaire, murCommentaireCompte } from './mur';
