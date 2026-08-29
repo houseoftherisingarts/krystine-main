5168b866720880b2322f40773cb5082cf1ec9d45
alextstlaurent <alex@lesalondesinconnus.com>
2026-08-28 21:08:42 -0400

    autosave: 2026-08-28 21:08:42

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { ShoppingBag, User, Music, Moon, Sun, Menu, X } from 'lucide-react';
 import { useUI, useAuth, useCart, useBoutique } from '../../contexts/AppContext';
+import Cloche from '../communaute/Cloche';
 
 interface NavItem { href: string; labelFR: string; labelEN: string; }
 
