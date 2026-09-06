455355bd4b16cb0c8f172cab5fb9a90f9fa4ad9a
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-06 17:53:37 -0400

    autosave: 2026-09-06 17:53:37

--- src/pages/BoutiqueCollectionPage.tsx
+++ src/pages/BoutiqueCollectionPage.tsx
@@ -388,6 +388,7 @@ const BoutiqueCollectionPage: React.FC = () => {
                 </div>
               </div>
             </div>
+            </Portail>
           );
         })()}
       </div>
