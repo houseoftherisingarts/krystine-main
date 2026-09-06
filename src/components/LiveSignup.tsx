5a3b7049d2ed2c967d26308f2b0d50a72ed5f27e
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-06 07:52:42 -0400

    autosave: 2026-09-06 07:52:42

--- src/components/LiveSignup.tsx
+++ src/components/LiveSignup.tsx
@@ -166,6 +166,7 @@ const LiveSignup: React.FC = () => {
                 ctaLabel={t.cta}
                 placeholder={t.placeholder}
                 success={{ title: t.okTitle, body: t.okBody }}
+                accountUpsell={false}
               />
             )}
           </motion.div>
