acc2214516d3a754a7a6f2deb74338020aad8c62
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-07 12:40:56 -0400

    autosave: 2026-09-07 12:40:56

--- src/pages/ClientPortal.tsx
+++ src/pages/ClientPortal.tsx
@@ -37,6 +37,7 @@ import '../components/client/skins.css';
 import Portail from '../components/Portail';
 import { suivreVerification, compterProgrammesSuivis, televerserPiece, demanderBadgeBleu, type Verification } from '../firebase/verification';
 import { SEUIL_PROGRAMMES } from '../lib/badgeBleu';
+import EditModeToggleButton from '../components/edit/EditModeToggleButton';
 
 // Le texte du niṣka, écrit par Alex le 6 septembre 2026, lu sous la bourse.
 const HISTOIRE_NISKA_FR = [
