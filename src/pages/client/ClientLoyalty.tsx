f783b737f8a44fd86ff0216a457b51a4496082fb
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-06 21:40:15 -0400

    autosave: 2026-09-06 21:40:15

--- src/pages/client/ClientLoyalty.tsx
+++ src/pages/client/ClientLoyalty.tsx
@@ -3,7 +3,7 @@ import { useApp } from '../../contexts/AppContext';
 import {
   subscribeToMemberPoints, getMemberPoints, listPointsEvents, listMyRewardRedemptions, redeemReward, points, reconcileBalance,
   type PointsBalance, type PointsEvent, type RewardRedemption, bienvenueDejaVersee } from '../../firebase/points';
-import { POINTS, TIERS, FACONS_DE_GAGNER, tierFromLifetime, rewardMinThreshold, niskas, journee, type Reward } from '../../lib/pointsConfig';
+import { POINTS, TIERS, FACONS_DE_GAGNER, tierFromLifetime, rewardMinThreshold, niskas, journee, veilleDe, type Reward } from '../../lib/pointsConfig';
 import { CYCLE_FOYER_MOIS, FOYER_HEBDO_JOURS, FOYER_MOIS_JOURS, FOYER_NISKAS_HEBDO_SI_MUSIQUE, prochainsCadeauxFoyer } from '../../lib/badgeBleu';
 import { useMembreDuFoyer } from '../../components/communaute/ReserveAuFoyer';
 import PieceNiska from '../../components/client/PieceNiska';
