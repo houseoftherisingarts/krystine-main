c2967527052a99bfc08c820811b7e7f821c2e93c
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-07 12:52:27 -0400

    autosave: 2026-09-07 12:52:26

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,4 +1,4 @@
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
@@ -12,6 +12,7 @@ import {
   onAuthStateChanged,
   User,
 } from 'firebase/auth';
+import { doc, setDoc, arrayUnion } from 'firebase/firestore';
 import { points } from './points';
 import { ensureMemberProfile } from './firestore';
 import { reclamerCodeRetenu } from './parrainage';
