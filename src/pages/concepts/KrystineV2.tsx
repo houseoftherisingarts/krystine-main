4dca7d77b4c731242dc6693961f5f4498cd20059
Krystine St-Laurent <krystine@inspiratanature.com>
2026-09-26 13:38:49 -0400

    Conférencière (/conferenciere) : chapitre 01, vraie photo de Mont-Tremblant 2024 en pleine conférence, plafond retiré au recadrage

--- src/pages/concepts/KrystineV2.tsx
+++ src/pages/concepts/KrystineV2.tsx
@@ -904,9 +904,9 @@ export default function KrystineV2() {
             >
               <img
                 data-beat-img
-                src="/conferences/mont-tremblant-2024-portrait.webp"
-                alt="Krystine St-Laurent sur scène devant une salle pleine, Mont-Tremblant, juin 2024"
-                className="absolute inset-0 h-[120%] w-full object-cover object-[70%_center] will-change-transform"
+                src="/conferences/mont-tremblant-2024-salle.webp"
+                alt="Krystine St-Laurent en conférence devant une salle pleine, Mont-Tremblant, juin 2024"
+                className="absolute inset-0 h-[120%] w-full object-cover object-center will-change-transform"
               />
             </div>
             <p
